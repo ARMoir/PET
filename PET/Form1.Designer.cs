13f945601adc0e2d082c41db2dc49f001811b57a
Alex Moir <AlexRMoir@gmail.com>
2020-08-17 21:39:09 -0400

    Add project files.

---
+++ PET/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Display = new System.Windows.Forms.ListBox();
+            this.Time = new System.Windows.Forms.Timer(this.components);
+            this.Feed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Display
+            // 
+            this.Display.Enabled = false;
+            this.Display.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Display.FormattingEnabled = true;
+            this.Display.Location = new System.Drawing.Point(12, 12);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(191, 147);
+            this.Display.TabIndex = 0;
+            // 
+            // Time
+            // 
+            this.Time.Enabled = true;
+            this.Time.Interval = 1000;
+            this.Time.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // Feed
+            // 
+            this.Feed.Location = new System.Drawing.Point(13, 166);
+            this.Feed.Name = "Feed";
+            this.Feed.Size = new System.Drawing.Size(75, 23);
+            this.Feed.TabIndex = 1;
+            this.Feed.Text = "Feed";
+            this.Feed.UseVisualStyleBackColor = true;
+            this.Feed.Click += new System.EventHandler(this.Feed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 240);
+            this.Controls.Add(this.Feed);
+            this.Controls.Add(this.Display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Display;
+        private System.Windows.Forms.Timer Time;
+        private System.Windows.Forms.Button Feed;
+    }
+}
+
---
+++ PET/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PET
+{
+    public partial class Form1 : Form
+    {
+
+        public int i = 0;
+        public int Full = 100;
+        public int Eating = 0;
+        public int Hour = 0;
+        public string Space = "Left";
+        public string NextSpace = "Left";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            i++;
+            Full--;
+            Space = NextSpace;
+
+            Hour = Int32.Parse(DateTime.Now.ToString("HH"));
+
+            if (Space == "Left")
+            {
+                BabyLeft();
+
+                NextSpace = "GoRight";
+            }
+
+            if (Space == "Right")
+            {
+                BabyRight();
+
+                NextSpace = "GoLeft";
+            }
+
+            if (Space == "GoLeft")
+            {
+                BabyMiddle();
+
+                NextSpace = "Left";
+
+            }
+
+            if (Space == "GoRight")
+            {
+                BabyMiddle();
+
+                NextSpace = "Right";
+            }
+
+            if (Eating > 0)
+            {
+               if (Eating == 1)
+                {
+                    BabyEat2();
+                    Eating--;
+                }
+                if (Eating == 2)
+                {
+                    BabyEat1();
+                    Eating--;
+                }
+                if (Eating == 3)
+                {
+                    BabyEat2();
+                    Eating--;
+                }
+            }
+
+
+            if (Hour > 20 || Hour < 8)
+            {
+                if (i % 2 == 0)
+                {
+                    BabySleep1();
+                }
+                else
+                {
+                    BabySleep2();
+                }
+            }
+
+            if (i == 1)
+            {
+                Egg();
+            }
+
+            if (i == 2)
+            {
+                Egg2();
+            }
+
+            if (i == 3)
+            {
+                Egg3();
+            }
+
+            if (i == 4)
+            {
+                Egg4();
+            }
+
+            if (Full < 1)
+            {
+                Dead();
+                return;
+            }
+
+            Status();
+        }
+
+        public void Status()
+        {
+            Display.Items.Add("Full: " + Full.ToString());
+           // Display.Items.Add("Time: " + Hour.ToString());
+        }
+
+        public void BabyLeft()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("");
+            Display.Items.Add("    ■ ■■■■ ■                  ");
+            Display.Items.Add("  ■          ■                ");
+            Display.Items.Add(" ■  ■      ■  ■               ");
+            Display.Items.Add(" ■    ■■■■    ■               ");
+            Display.Items.Add(" ■            ■               ");
+            Display.Items.Add("  ■          ■                ");
+            Display.Items.Add("    ■ ■■■■ ■                  ");
+            Display.Items.Add("");
+            Display.Items.Add("");
+
+        }
+
+        public void BabyRight()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("");
+            Display.Items.Add("                  ■ ■■■■ ■    ");
+            Display.Items.Add("                ■          ■  ");
+            Display.Items.Add("               ■  ■      ■  ■ ");
+            Display.Items.Add("               ■    ■■■■    ■ ");
+            Display.Items.Add("               ■            ■ ");
+            Display.Items.Add("                ■          ■  ");
+            Display.Items.Add("                  ■ ■■■■ ■    ");
+            Display.Items.Add("");
+            Display.Items.Add("");
+        }
+
+        public void BabyMiddle()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("");
+            Display.Items.Add("");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("        ■  ■■    ■■  ■        ");
+            Display.Items.Add("        ■    ■■■■    ■        ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("");
+            Display.Items.Add("");
+        }
+
+        public void BabySleep1()
+        {
+            Display.Items.Clear();
+            Display.Items.Add(" ■■■■■                        ");
+            Display.Items.Add("    ■                         ");
+            Display.Items.Add("   ■       ■ ■■■■ ■           ");
+            Display.Items.Add("  ■      ■          ■         ");
+            Display.Items.Add(" ■■■■■  ■  ■■    ■■  ■        ");
+            Display.Items.Add("        ■    ■■■■    ■        ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("                              ");
+            Display.Items.Add("                              ");
+        }
+
+        public void BabySleep2()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("                              ");
+            Display.Items.Add("                              ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("        ■  ■■    ■■  ■        ");
+            Display.Items.Add("        ■    ■■■■    ■  ■■■■■ ");
+            Display.Items.Add("         ■          ■      ■  ");
+            Display.Items.Add("           ■ ■■■■ ■       ■   ");
+            Display.Items.Add("                         ■    ");
+            Display.Items.Add("                        ■■■■■ ");
+        }
+
+        public void BabyEat1()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("        ■  ■      ■  ■        ");
+            Display.Items.Add("        ■    ■■■■    ■        ");
+            Display.Items.Add("        ■            ■        ");
+            Display.Items.Add("         ■   ■■■■   ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("");
+            Display.Items.Add("");
+        }
+
+        public void BabyEat2()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("        ■  ■      ■  ■        ");
+            Display.Items.Add("        ■    ■■■■    ■        ");
+            Display.Items.Add("        ■    ■■■■    ■        ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+            Display.Items.Add("");
+            Display.Items.Add("");
+        }
+
+        public void Egg()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("              ■■              ");
+            Display.Items.Add("            ■    ■            ");
+            Display.Items.Add("          ■        ■          ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("        ■            ■        ");
+            Display.Items.Add("       ■              ■       ");
+            Display.Items.Add("       ■              ■       ");
+            Display.Items.Add("       ■              ■       ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+        }
+
+        public void Egg2()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("             ■■               ");
+            Display.Items.Add("           ■    ■             ");
+            Display.Items.Add("         ■        ■           ");
+            Display.Items.Add("        ■          ■          ");
+            Display.Items.Add("       ■             ■        ");
+            Display.Items.Add("      ■               ■       ");
+            Display.Items.Add("      ■               ■       ");
+            Display.Items.Add("       ■              ■       ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+        }
+
+        public void Egg3()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("               ■■             ");
+            Display.Items.Add("             ■    ■           ");
+            Display.Items.Add("           ■        ■         ");
+            Display.Items.Add("         ■           ■        ");
+            Display.Items.Add("        ■             ■       ");
+            Display.Items.Add("       ■               ■      ");
+            Display.Items.Add("       ■               ■      ");
+            Display.Items.Add("       ■              ■       ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+        }
+
+        public void Egg4()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("              ■■              ");
+            Display.Items.Add("            ■    ■            ");
+            Display.Items.Add("          ■        ■          ");
+            Display.Items.Add("       ■  ■ ■ ■■ ■ ■  ■       ");
+            Display.Items.Add("        ■  ■      ■  ■        ");
+            Display.Items.Add("        ■    ■■■■    ■        ");
+            Display.Items.Add("       ■  ■ ■ ■■ ■ ■  ■       ");
+            Display.Items.Add("       ■              ■       ");
+            Display.Items.Add("         ■          ■         ");
+            Display.Items.Add("           ■ ■■■■ ■           ");
+        }
+
+
+        public void Dead()
+        {
+            Display.Items.Clear();
+            Display.Items.Add("            ■ ■ ■ ■           ");
+            Display.Items.Add("            ■     ■           ");
+            Display.Items.Add("      ■ ■ ■ ■     ■ ■ ■ ■     ");
+            Display.Items.Add("      ■      R.I.P.     ■     ");
+            Display.Items.Add("      ■ ■ ■ ■     ■ ■ ■ ■     ");
+            Display.Items.Add("            ■     ■           ");
+            Display.Items.Add("            ■     ■           ");
+            Display.Items.Add("            ■     ■           ");
+            Display.Items.Add("            ■     ■           ");
+            Display.Items.Add("            ■ ■ ■ ■           ");
+        }
+
+        private void Feed_Click(object sender, EventArgs e)
+        {
+            Full += 5;
+            Eating = 3;
+        }
+    }
+}
