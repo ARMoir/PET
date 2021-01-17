fd742c9f5070a459f5b29313abb666eeba37c564
Alex Moir <alexrmoir@gmail.com>
2021-01-17 14:02:53 -0500

    2.1.1.17

--- PET/Form1.Designer.cs
+++ PET/Form1.Designer.cs
@@ -41,9 +41,11 @@
             // 
             this.Display.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Display.FormattingEnabled = true;
-            this.Display.Location = new System.Drawing.Point(12, 12);
+            this.Display.ItemHeight = 17;
+            this.Display.Location = new System.Drawing.Point(16, 15);
+            this.Display.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Display.Name = "Display";
-            this.Display.Size = new System.Drawing.Size(200, 134);
+            this.Display.Size = new System.Drawing.Size(265, 174);
             this.Display.TabIndex = 0;
             // 
             // Time
@@ -54,9 +56,10 @@
             // 
             // Feed
             // 
-            this.Feed.Location = new System.Drawing.Point(13, 214);
+            this.Feed.Location = new System.Drawing.Point(16, 275);
+            this.Feed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Feed.Name = "Feed";
-            this.Feed.Size = new System.Drawing.Size(100, 25);
+            this.Feed.Size = new System.Drawing.Size(133, 31);
             this.Feed.TabIndex = 1;
             this.Feed.Text = "Feed";
             this.Feed.UseVisualStyleBackColor = true;
@@ -64,9 +67,10 @@
             // 
             // Clean
             // 
-            this.Clean.Location = new System.Drawing.Point(113, 214);
+            this.Clean.Location = new System.Drawing.Point(149, 275);
+            this.Clean.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Clean.Name = "Clean";
-            this.Clean.Size = new System.Drawing.Size(100, 25);
+            this.Clean.Size = new System.Drawing.Size(133, 31);
             this.Clean.TabIndex = 2;
             this.Clean.Text = "Clean";
             this.Clean.UseVisualStyleBackColor = true;
@@ -75,16 +79,19 @@
             // StatusBox
             // 
             this.StatusBox.FormattingEnabled = true;
-            this.StatusBox.Location = new System.Drawing.Point(13, 152);
+            this.StatusBox.ItemHeight = 16;
+            this.StatusBox.Location = new System.Drawing.Point(16, 199);
+            this.StatusBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StatusBox.Name = "StatusBox";
-            this.StatusBox.Size = new System.Drawing.Size(200, 56);
+            this.StatusBox.Size = new System.Drawing.Size(265, 68);
             this.StatusBox.TabIndex = 3;
             // 
             // Medicine
             // 
-            this.Medicine.Location = new System.Drawing.Point(12, 241);
+            this.Medicine.Location = new System.Drawing.Point(16, 314);
+            this.Medicine.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Medicine.Name = "Medicine";
-            this.Medicine.Size = new System.Drawing.Size(100, 25);
+            this.Medicine.Size = new System.Drawing.Size(133, 31);
             this.Medicine.TabIndex = 4;
             this.Medicine.Text = "Medicine";
             this.Medicine.UseVisualStyleBackColor = true;
@@ -92,16 +99,17 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(220, 278);
+            this.ClientSize = new System.Drawing.Size(293, 358);
             this.Controls.Add(this.Medicine);
             this.Controls.Add(this.StatusBox);
             this.Controls.Add(this.Clean);
             this.Controls.Add(this.Feed);
             this.Controls.Add(this.Display);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Pet";
             this.ResumeLayout(false);
 
         }
